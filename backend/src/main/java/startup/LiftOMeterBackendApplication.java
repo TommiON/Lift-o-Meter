42715298335986699fc6353849dd0b7c353f4a2d
Tommi Niittymies <tommi.niittymies@gmail.com>
2020-08-15 09:38:56 +0300

    Backend and frontend configurations, planning documentation

---
+++ backend/src/main/java/startup/LiftOMeterBackendApplication.java
@@ -0,0 +1,13 @@
+package startup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftOMeterBackendApplication {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Lift-o-Meter backend starting up...");
+        SpringApplication.run(LiftOMeterBackendApplication.class, args);
+    }
+}
