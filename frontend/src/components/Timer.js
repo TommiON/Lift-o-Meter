652dc3b03056a82daa450b49905ec1fe6d8a253d
Tommi Niittymies <tommi.niittymies@gmail.com>
2020-08-16 22:23:24 +0300

    More frontend work with Timer component

--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -3,20 +3,26 @@ import React, { useState } from 'react'
 const Timer = ({startTime, headsUpTime}) => {
     const [clock, setClock] = useState(startTime)
     
-    const getSeconds = rawTimeInSeconds => {
-        let seconds = rawTimeInSeconds % 60
+    setTimeout(() => setClock(clock - 1), 1000)
+
+    const getSeconds = rawTime => {
+        let seconds = rawTime % 60
         if (seconds < 10) {
             seconds = ('0' + seconds).slice(-2)
         }
         return seconds
     }
     
-    const getMinutes = rawTimeInSeconds => Math.floor(rawTimeInSeconds / 60)
-    
-    setTimeout(() => setClock(clock - 1), 1000)
+    const getMinutes = rawTime => Math.floor(rawTime / 60)
 
+    const textStyle = {
+        color: 'green',
+        fontStyle: 'bold',
+        fontSize: 16
+    }
+    
     return(
-        <div>
+        <div style={textStyle}>
             {(clock > headsUpTime)
                 ? <h3>{getMinutes(clock)} : {getSeconds(clock)}</h3>
                 : (clock > 0 && clock <= headsUpTime)
