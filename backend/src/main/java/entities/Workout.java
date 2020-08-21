74bb01f0a64716a855fd09d5491641953176c68c
Tommi Niittymies <tommi.niittymies@gmail.com>
2020-08-21 13:19:22 +0300

    Backend: class graph for Entities, implementation of Entity classes

--- backend/src/main/java/entities/Workout.java
+++ backend/src/main/java/entities/Workout.java
@@ -3,12 +3,10 @@ package entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.jpa.domain.AbstractPersistable;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.Date;
@@ -17,21 +15,17 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Cycle extends AbstractPersistable<Long> {
+public class Workout {
 
     @NotNull
     @ManyToOne
-    private UserAccount trainer;
+    private Cycle cycle;
 
-    @OneToMany(mappedBy = "cycle")
-    private ArrayList<Workout> workouts;
-
-    @FutureOrPresent
-    private Date startDate;
-
-    @FutureOrPresent
-    private Date endDate;
+    // konstruktori hoitaa populoimisen, saa parametrina edellisen Workoutin ja päättelee siitä seuraavan?
+    @OneToMany(mappedBy = "workout")
+    private ArrayList<MoveSet> sets;
 
     @NotNull
-    private boolean active;
+    private Date date;
+
 }
